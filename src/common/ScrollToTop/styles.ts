8d1b41c274fcc58eebe0f2c61133efece45de00a
Wellington <wellington.wong@exults.com>
2026-04-08 23:03:49 +0800

    Center scroll to top and add website portfolio images

--- src/common/ScrollToTop/styles.ts
+++ src/common/ScrollToTop/styles.ts
@@ -5,14 +5,14 @@ export const ScrollUpContainer = styled("div")<{
 }>`
   padding: 10px;
   position: fixed;
-  right: 30px;
+  left: 50%;
   bottom: 30px;
   z-index: 10;
   cursor: pointer;
   background: rgb(241, 242, 243);
   text-align: center;
   align-items: center;
-  border-radius: 4px;
+  border-radius: 5px;
   transition: all 0.3s ease-in-out;
   visibility: ${(p) => (p.show ? "visible" : "hidden")};
   opacity: ${(p) => (p.show ? "1" : "0")};
