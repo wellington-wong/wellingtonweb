bc110cd1f6f8cc2696596e6eb377ad1f432e076d
Wellington <wellington.wong@exults.com>
2026-04-17 03:29:56 +0800

    Update contact form

--- src/common/Input/index.tsx
+++ src/common/Input/index.tsx
@@ -1,6 +1,6 @@
 import { withTranslation } from "react-i18next";
 import { Container, StyledInput } from "./styles";
-import { Label } from "../TextArea/styles";
+//import { Label } from "../TextArea/styles";
 import { InputProps } from "../types";
 
 const Input = ({ name, placeholder, onChange, t }: InputProps) => (
--- src/common/TextArea/index.tsx
+++ src/common/TextArea/index.tsx
@@ -1,5 +1,5 @@
 import { withTranslation } from "react-i18next";
-import { StyledTextArea, StyledContainer, Label } from "./styles";
+import { StyledTextArea, StyledContainer } from "./styles";
 import { InputProps } from "../types";
 
 const TextArea = ({ name, placeholder, onChange, t }: InputProps) => (
