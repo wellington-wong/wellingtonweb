169ed025627a22a4552b728ad575d3b8cd674353
Wellington <wellington.wong@exults.com>
2026-04-26 05:16:57 +0800

    Update intro section button labels; Fix and cleanup footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,52 +1,18 @@
 import { Row, Col } from "antd";
 import { withTranslation, TFunction } from "react-i18next";
-//import { SvgIcon } from "../../common/SvgIcon";
 import Container from "../../common/Container";
 import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
 
-//import i18n from "i18next";
 import {
   Copyright,
   FooterSection,
-  //Title,
-  //NavLink,
   Extra,
-  //LogoContainer,
   Para,
-  //Large,
-  //Chat,
-  //Empty,
-  //FooterContainer,
   Language,
-  //Label,
-  //LanguageSwitch,
-  //LanguageSwitchContainer,
   NewLink,
 } from "./styles";
 
-/*interface SocialLinkProps {
-  href: string;
-  src: string;
-}*/
-
 const Footer = ({ t }: { t: TFunction }) => {
-  /*const handleChange = (language: string) => {
-    i18n.changeLanguage(language);
-  };*/
-
-  /*const SocialLink = ({ href, src }: SocialLinkProps) => {
-    return (
-      <a
-        href={href}
-        target="_blank"
-        rel="noopener noreferrer"
-        key={src}
-        aria-label={src}
-      >
-        <SvgIcon src={src} width="25px" height="25px" />
-      </a>
-    );
-  };*/
 
   return (
     <>
@@ -56,7 +22,7 @@ const Footer = ({ t }: { t: TFunction }) => {
             <Col lg={10} md={10} sm={12} xs={12}>
               <Language>{t("Contact Me")}</Language>
               <Para>
-                {t(`Do you have any question? Feel free to react out.`)}
+                {t(`Do you have any question? Feel free to reach out.`)}
               </Para>
               <Para> 
                 <NewLink href="mailto:wellingtonwong@gmail.com">
@@ -79,25 +45,6 @@ const Footer = ({ t }: { t: TFunction }) => {
               <Copyright>© 2026 Wellington Webpages</Copyright>
             </Col>
           </Row>
-          {/*<Row justify="space-between">
-            <Col lg={10} md={10} sm={12} xs={12}>
-              <Title>{t("Policy")}</Title>
-              <Large to="/">{t("Application Security")}</Large>
-              <Large to="/">{t("Software Principles")}</Large>
-            </Col>
-            <Col lg={8} md={8} sm={12} xs={12}>
-              <Title>{t("Company")}</Title>
-              <Large to="/">{t("About")}</Large>
-              <Large to="/">{t("Blog")}</Large>
-              <Large to="/">{t("Press")}</Large>
-              <Large to="/">{t("Careers & Culture")}</v>
-            </Col>
-            <Col lg={6} md={6} sm={12} xs={12}>
-              <Empty />
-              <Large to="/">{t("Support Center")}</Large>
-              <Large to="/">{t("Customer Support")}</Large>
-            </Col>
-          </Row>*/}
         </Container>
       </FooterSection>
       <Extra>
@@ -105,56 +52,6 @@ const Footer = ({ t }: { t: TFunction }) => {
             propertyId="69d40991b0cccb1c3b0d06e6"
             widgetId="1jlnah6r7"/>
       </Extra>
-      {/*
-        <Container border={true}>
-          <Row
-            justify="space-between"
-            align="middle"
-            style={{ paddingTop: "3rem" }}
-          >
-            <NavLink to="/">
-              <LogoContainer>
-                <SvgIcon
-                  src="logo.svg"
-                  aria-label="homepage"
-                  width="101px"
-                  height="64px"
-                />
-              </LogoContainer>
-            </NavLink>
-            <FooterContainer>
-              <SocialLink
-                href="https://github.com/Adrinlol/create-react-app-adrinlol"
-                src="github.svg"
-              />
-              <SocialLink
-                href="https://twitter.com/Adrinlolx"
-                src="twitter.svg"
-              />
-              <SocialLink
-                href="https://www.linkedin.com/in/lasha-kakabadze/"
-                src="linkedin.svg"
-              />
-              <SocialLink
-                href="https://medium.com/@lashakakabadze/"
-                src="medium.svg"
-              />
-              <a
-                href="https://ko-fi.com/Y8Y7H8BNJ"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  height="36"
-                  style={{ border: 0, height: 36 }}
-                  src="https://storage.ko-fi.com/cdn/kofi3.png?v=3"
-                  alt="Buy Me a Coffee at ko-fi.com"
-                />
-              </a>
-            </FooterContainer>
-          </Row>
-        </Container>
-      */}
     </>
   );
 };
